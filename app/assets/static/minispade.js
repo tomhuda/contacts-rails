7f4b07375c892418db7d3bfd4960575b5f3513fe
tomhuda <tomhuda@tilde.io>
2012-02-20 17:26:58 -0800

    Add JavaScript bootstrap HTML

---
+++ app/assets/static/minispade.js
@@ -0,0 +1,53 @@
+/*jshint evil:true*/
+/*global minispade:true*/
+
+if (typeof document !== "undefined") {
+  (function() {
+    window.minispade = {
+      root: null,
+      modules: {},
+      loaded: {},
+
+      globalEval: function(data) {
+        if ( data ) {
+          // We use execScript on Internet Explorer
+          // We use an anonymous function so that context is window
+          // rather than jQuery in Firefox
+          ( window.execScript || function( data ) {
+            window[ "eval" ].call( window, data );
+          } )( data );
+        }
+      },
+
+      require: function(name) {
+        var loaded = minispade.loaded[name];
+        var mod = minispade.modules[name];
+
+        if (!loaded) {
+          if (mod) {
+            minispade.loaded[name] = true;
+
+            if (typeof mod === "string") {
+              this.globalEval(mod);
+            } else {
+              mod();
+            }
+          } else {
+            if (minispade.root && name.substr(0,minispade.root.length) !== minispade.root) {
+              return minispade.require(minispade.root+name);
+            } else {
+              throw "The module '" + name + "' could not be found";
+            }
+          }
+        }
+
+        return loaded;
+      },
+
+      register: function(name, callback) {
+        minispade.modules[name] = callback;
+      }
+    };
+  })();
+}
+
